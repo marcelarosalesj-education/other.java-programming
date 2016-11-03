98140105f17bdf64d36599d017a6e3e61ff19d6d
Marcela Rosales <marcelarosalesj@gmail.com>
2016-11-03 17:56:54 -0600

    Added DisplayFamilyTree function. For now it only display direct descendants of a root, it doesn't show any other relationships.
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- FamilyTree/FamilyTree.java
+++ FamilyTree/FamilyTree.java
@@ -6,22 +6,33 @@ public class FamilyTree {
 	public static void main(String[] args) { 
         System.out.println("Welcome to the Family Tree Creator!");
                 
-        Human firstHuman = new Human("p1", LocalDate.of(1980, Month.MARCH, 2), null);
-        Human secondHuman = new Human("p2", LocalDate.of(1960, Month.JUNE, 14), null);
+        Human h1 = new Human("h1", LocalDate.of(1980, Month.JANUARY, 1), null);
+        Human h2 = new Human("h2", LocalDate.of(1970, Month.FEBRUARY, 5), null);
+        Human h3 = new Human("h3", LocalDate.of(1960, Month.MARCH, 11), null);
+        Human h4 = new Human("h4", LocalDate.of(1950, Month.APRIL, 14), null);
+        Human h5 = new Human("h5", LocalDate.of(1940, Month.MAY, 23), null);
+        Human h6 = new Human("h6", LocalDate.of(1930, Month.JUNE, 27), null);
+        Human h7 = new Human("h7", LocalDate.of(1935, Month.MARCH, 13), null);
+        Human h8 = new Human("h8", LocalDate.of(1945, Month.JULY, 15), null);
+        Human h9 = new Human("h9", LocalDate.of(1955, Month.DECEMBER, 16), null);
+        Human h10 = new Human("h10", LocalDate.of(1965, Month.OCTOBER, 17), null);        
+
+        h1.addDescendant(h3);
+        h1.addDescendant(h4);
+
+        h2.addDescendant(h3);
+        h2.addDescendant(h5);
+
+        h3.addDescendant(h9);
+        h3.addDescendant(h10);
         
-        secondHuman.addDescendant(firstHuman);
-        System.out.print(firstHuman.name + "\t");
-        System.out.println(firstHuman.birthdate);
-        System.out.print(secondHuman.name + "\t");
-        System.out.println(secondHuman.birthdate);
-        System.out.println("Children of " + secondHuman.name + " :");
-        secondHuman.displayDescendants();
-	
-                firstHuman.addParent(secondHuman);
+        h4.addDescendant(h6);
+        h4.addDescendant(h7);
+        h4.addDescendant(h8);
 
-        System.out.println("Parent of " + firstHuman.name + " :");
-        firstHuman.displayParents();
-
+        System.out.println("- Display Tree -");
+        h1.displayFamilyTree(0);
+        h2.displayFamilyTree(0);
 
         }
 }
--- FamilyTree/Human.java
+++ FamilyTree/Human.java
@@ -6,36 +6,35 @@ public class Human {
 	public String name;
 	LocalDate birthdate;
 	LocalDate deathdate;
-	Human parentOne;
-	Human parentTwo;
+	Human[] parent = new Human[2];
 	ArrayList<Human> descendant = new ArrayList<Human>();
 
 	private static int nextId=0;
 	
 	public Human(String name, LocalDate birthdate, LocalDate deathdate) {
-		super();
 		this.id = nextId;
 		nextId++;
 		this.name = name;
 		this.birthdate = birthdate;
 		this.deathdate = deathdate;
-		// Add empty parent Human
-		// Add empty parent Human
+		this.parent[0] = null;
+		this.parent[1] = null;
+		descendant.clear();
 	}
 	
 	public Human(String name, LocalDate birthdate) {
-		super();
 		this.id = nextId;
 		nextId++;
 		this.name = name;
 		this.birthdate = birthdate;
 		this.deathdate = null;
-		// Add empty parent Human
-		// Add empty parent Human
+		this.parent[0] = null;
+		this.parent[1] = null;
+		descendant.clear();
 	}
 	
 	public void addDescendant(Human child){
-		this.descendant.add(child);
+		child.addParent(this);
 	}
 	
 	public void displayDescendants(){
@@ -43,23 +42,60 @@ public class Human {
 			System.out.println(i.name);
 	}
 	
-	public void addParent(Human parent){
-		if(this.parentOne == null){
-			this.parentOne =  parent; 
-		} else if (this.parentTwo == null){
-			this.parentTwo = parent;
+	public void addParent(Human par){
+		if(this.parent[0] == null){
+			this.parent[0] =  par;
+			par.descendant.add(this);
+		} else if (this.parent[1] == null){
+			this.parent[1] = par;
+			par.descendant.add(this);
 		} else {
 			System.out.println("Already has two parents. Maybe you want to remove one.");
 		}
 	}
 
 	public void displayParents(){
-		if(this.parentOne != null)
-		System.out.println(this.parentOne.name);
-		if(this.parentTwo != null)
-		System.out.println(this.parentTwo.name);
-
+		if(this.parent[0] != null)
+			System.out.println(this.parent[0].name);
+		if(this.parent[1] != null)
+			System.out.println(this.parent[1].name);
 	}
-	
+
+	public void deleteParent(int num){
+		if(num == 1){
+			// Delete parent one relationship
+			this.parent[0] = null;
+		} else if(num == 2){
+			// Delete parent two relationship
+			this.parent[1] = null;
+		} else {
+			System.out.println("Not a valid parent id.");
+		}
+	}
+
+	public void displayFamilyTree(int spaces){
+		if( !this.hasChildren() ){
+			for(int s=0; s<spaces; s++){ System.out.print("   ");}
+			System.out.println(this.name);
+			return;
+		} else {
+			for(int s=0; s<spaces; s++){ System.out.print("   ");}
+			System.out.println(this.name);
+			spaces+=1;
+			for(Human hum : descendant){
+				hum.displayFamilyTree(spaces);
+
+			}	
+		}
+			
+	}
+
+	public boolean hasChildren(){
+		if( descendant.isEmpty() ){
+			return false;
+		} else {
+			return true;
+		}
+	}
 	
 }
